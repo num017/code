3720ea58be0d05b2d8e5856252f5ae514346286d
num017 <jp08076780410@gmail.com>
2022-02-21 21:23:47 +0800

    change 1

---
+++ move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class move1 : MonoBehaviour
+{
+    public Rigidbody2D Pl;
+    public float speed;
+    public GameObject p2;
+
+    void Update()
+    {
+        Move();
+        if(p2==false)
+        {
+            SceneManager.LoadScene("again1");
+        }
+    }
+
+    void Move()
+    {
+        float horizontalmove = Input.GetAxisRaw("Horizontal");
+        Pl.velocity = new Vector2(horizontalmove * speed, Pl.velocity.y);
+        if (horizontalmove != 0)
+        {
+            transform.localScale = new Vector3(horizontalmove, 1, 1);
+        }
+    }
+
+}
